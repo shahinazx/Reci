97cdd415cbbeb8846edee689230b10617135893f
shahinaz <shahinazalsubaie@gmail.com>
2024-10-24 14:34:24 +0300

    Initial Commit

---
+++ Reci/ReciApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciApp.swift
+//  Reci
+//
+//  Created by Shahinaz Alsubaie on 21/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ReciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReciTests/ReciTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReciTests.swift
+//  ReciTests
+//
+//  Created by Shahinaz Alsubaie on 21/04/1446 AH.
+//
+
+import Testing
+@testable import Reci
+
+struct ReciTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
